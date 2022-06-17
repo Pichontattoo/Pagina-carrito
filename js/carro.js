196df98ccc3e51b81ffefa4f1929c40d10416747
Bernabe <bernatattoo@gmail.com>
2022-06-16 23:22:56 -0300

    renderizar carrito y suma total

--- js/carro.js
+++ js/carro.js
@@ -1,22 +1,42 @@
 
 const contenedorCarro = document.getElementById('container-carro')
+const contenedor = document.getElementById('contenedor')
+const data = JSON.parse(localStorage.getItem('baseDeDatos'))
+const templateCarro = document.getElementById('renderCarro').content
 const fragmento = document.createDocumentFragment()
+const valorTotal = document.getElementById('totalCompra')
 
 window.addEventListener('load', () => {  
-    const contenedor = document.getElementById('contenedor')
-    const div = document.createElement('div')
-    const data = JSON.parse(localStorage.getItem('baseDeDatos'))
-
-   data && data.map(item => {
-        const h4 = document.createElement('h4')
-        const img = document.createElement('img')
-        const h6 = document.createElement('h6')
-        h4.appendChild(document.createTextNode(item.name))
-        h6.appendChild(document.createTextNode(item.price))
-        img.src = item.image
-        div.appendChild(h4)
-        div.appendChild(h6)
-        div.appendChild(img)
-        contenedor.appendChild(div)
+    const carrito = data.filter(item => [{ name: item.name, image: item.image, price: item.price}])
+        console.log(carrito)
+        imprimirCarro(carrito)
     })
-})
+
+
+const imprimirCarro = data => {
+    data.map(product => {
+  
+      templateCarro.querySelector('h4').textContent = product.name
+      templateCarro.querySelector('p').textContent = product.price
+      const clonar = templateCarro.cloneNode(true);
+      fragmento.appendChild(clonar)
+  
+    })
+    contenedorCarro.appendChild(fragmento)
+  }
+
+  // sumar los precios de los productos
+
+const totalCompraCarro = () => {
+    let totalCompra = 0
+  
+    for(let item in data){
+      totalCompra += data[item].price * 1
+    }
+    
+    valorTotal.innerHTML = `Total: $ ${totalCompra}`
+    console.log(totalCompra)
+  } 
+
+  totalCompraCarro()
+
